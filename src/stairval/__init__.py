61844c732fea382b444bdb0fa7f86646d1cee892
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-08-21 21:11:30 +0200

    Implement prototype API.

--- src/stairval/__init__.py
+++ src/stairval/__init__.py
@@ -2,5 +2,11 @@
 Stairval is a framework for validating hierarchical data structures.
 """
 
+
+from ._auditor import Auditor, Notepad, Issue, Level
+
 __version__ = "0.1.0.dev0"
 
+__all__ = [
+    "Auditor", "Notepad", "Issue", "Level",
+]
