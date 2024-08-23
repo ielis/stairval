14f4c9aac55aa0e46f0c3fd9633457aba0cb6830
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-08-23 10:21:11 +0200

    Next development iteration `0.2.1.dev0`.

--- src/stairval/__init__.py
+++ src/stairval/__init__.py
@@ -5,7 +5,7 @@ Stairval is a framework for validating hierarchical data structures.
 from ._api import Level, Issue
 from ._auditor import Auditor, ITEM
 
-__version__ = "0.2.0"
+__version__ = "0.2.1.dev0"
 
 __all__ = [
     "Auditor", "Issue", "Level", "ITEM",
