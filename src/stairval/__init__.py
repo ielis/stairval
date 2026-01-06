b2d742b23675d8f05d5d671b4ce81aa8e01f53a7
Daniel Danis <daniel.gordon.danis@protonmail.com>
2026-01-06 15:37:31 +0100

    Make release `0.2.2`.

--- src/stairval/__init__.py
+++ src/stairval/__init__.py
@@ -2,10 +2,12 @@
 Stairval is a framework for validating hierarchical data structures.
 """
 
+from importlib.metadata import version
+
 from ._api import Level, Issue
 from ._auditor import Auditor, ITEM
 
-__version__ = "0.2.2.dev0"
+__version__ = version("stairval")
 
 __all__ = [
     "Auditor",
